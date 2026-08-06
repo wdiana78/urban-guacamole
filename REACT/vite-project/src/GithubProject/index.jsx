4e4498e39bb2dcd2b273b52553807248a85a76c3
Diana Mwangi <wanjikudiana99@gmail.com>
2026-08-07 00:12:35 +0300

    Update index.jsx

--- REACT/vite-project/src/GithubProject/index.jsx
+++ REACT/vite-project/src/GithubProject/index.jsx
@@ -1,13 +1,24 @@
 /*
-// This is ONE user returned by GitHub
-
-const person = {
+person =
+{
   login: "mojombo",
   id: 1,
-  avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-  html_url: "https://github.com/mojombo",
-  type: "User",
-};
+  avatar_url: "...",
+  html_url: "...",
+  type: "User"
+}
+
+
+
+
+person =
+{
+  login: "defunkt",
+  id: 2,
+  avatar_url: "...",
+  html_url: "...",
+  type: "User"
+}
 */
 
 
