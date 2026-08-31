c25caf663030ffed15f86e77ed7d8ca1dae4e2a8
Diana Mwangi <wanjikudiana99@gmail.com>
2026-08-31 13:34:40 +0300

    Update args_kwargs.py

--- PYTHON/Intro/args_kwargs.py
+++ PYTHON/Intro/args_kwargs.py
@@ -9,3 +9,8 @@ def args_kwargs(*args,**kwargs):
 #args_kwargs(a=2,b=30,45,39)
 
 args_kwargs(45,39,a=2,b=30)
+
+
+#nb
+*args   → (45, 39)              # tuple
+**kwargs → {"a": 2, "b": 30}   # dictionary
