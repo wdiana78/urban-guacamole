6b75e82fba3766d49331d1480c358eb0638e67b9
Diana Mwangi <wanjikudiana99@gmail.com>
2026-08-31 15:22:24 +0300

    Add logger decorator

---
+++ PYTHON/decorators/logger_project/logger_decorator.py
@@ -0,0 +1,46 @@
+# → Log decorator function
+# → create a file based the current date
+# → function called , time called, time taken, result→
+# → decorators →
+# → python file system <>in built.
+# → python time.
+
+import time
+from datetime import date, datetime
+
+
+def logger(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        print(f"start time {start_time}")
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        diff = end_time - start_time
+        diff_in_ms = round(diff * 1000, 2)
+        today = date.today()
+        timestamp = datetime.now().isoformat()
+
+        with open(f"{today}.txt", "a") as f:
+            f.write(
+                f"ts:{timestamp}--func:{func.__name__}--time:{diff_in_ms}.ms\n"
+            )
+
+        return result
+
+    return wrapper
+
+
+@logger
+def hello():
+    print("Hello world")
+
+
+@logger
+def big_count():
+    for i in range(0, 100):
+        print(i)
+        pass
+
+
+hello()
+big_count()
